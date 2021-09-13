a321dc41169f7312514267dce6094cb30fc1ceda
Lucas Leon Labanca <lucas.labanca@inatel.br>
2021-09-13 14:56:04 -0300

    Authentication challenge done and working inside aula2/autenticacao

--- aula-2/autenticacao/rest-server-mongoose/src/middleware/check-auth.js
+++ aula-2/autenticacao/rest-server-mongoose/src/middleware/check-auth.js
@@ -1,19 +1,35 @@
+const User = require('../models/user');
+
 const checkAuth = (request, response, next) => {
-  const token = request.headers.authorization;
+  const token = request.headers.authorization.split(' ')[1];
+  const credentials = Buffer.from(token, 'base64').toString('ascii');
+  const [email, password] = credentials.split(':');
 
-  if(/ZG0xMjQ6bm90VzM0a1BAc3M=/.test(token)) {
-    next();
-  } else {
-    const HttpStatusNotAuthorized = 401;
-    const errorInfo = {
-      status: HttpStatusNotAuthorized,
-      message: 'Not authorized'
-    };
+  User.findOne({ 'email': email })
+  .then(user => {
+    if (!user) notAuthorized();
+    
+    if (password == user.password)
+      next(); 
+    else 
+      notAuthorized(response);
+   
+  }) 
+  .catch(err => {
+    notAuthorized(response);
+  });
+}
 
-    response
-      .status(HttpStatusNotAuthorized)
-      .json(errorInfo);
-  }
+const notAuthorized = (response) => {
+  const HttpStatusNotAuthorized = 401;
+  const errorInfo = {
+    status: HttpStatusNotAuthorized,
+    message: 'Not authorized'
+  };
+
+  response
+    .status(HttpStatusNotAuthorized)
+    .json(errorInfo);
 }
 
 module.exports = checkAuth;
--- aula-2/autenticacao/rest-server-mongoose/src/models/user.js
+++ aula-2/autenticacao/rest-server-mongoose/src/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 	const userSchema = new Schema({
 		email: {
 			type: String,
-			required: true
+			required: true,
+			unique: true
 		},
 		password: {
 			type: String,
